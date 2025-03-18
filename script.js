307b4df895f66a2c9fd6436d2c116a5f340a184b
parag lakum <paragl.albiorix@gmail.com>
2025-03-18 11:34:11 +0530

    task-2

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function changeText() {
-    
+function NewText() {
     const text = document.querySelector("#text")
     text.innerHTML = "This is New Paragraph"
+}
+function OldText() {
+    document.querySelector("#text").textContent = "This is Old Paragraph"
+
+}
+
+
+function addTask() {
+    const ul = document.getElementById("list")
+    const li = document.createElement("li")
+    li.textContent = "New Task"
+    ul.appendChild(li);
 }
