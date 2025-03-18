4c524cf14fc8ce21e5c0d1df51e6aa96959cb12a
parag lakum <paragl.albiorix@gmail.com>
2025-03-18 11:17:21 +0530

    Task-1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeText() {
-    // document.getElementById("text").innerText = "This is New Paragraph"
+    
     const text = document.querySelector("#text")
     text.innerHTML = "This is New Paragraph"
 }
